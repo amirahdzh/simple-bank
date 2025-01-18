084be07aa569ba4c5b8dfde647cca21c891c85c6
amirahdzh <amirahdzh@gmail.com>
2025-01-18 10:00:31 +0700

    initialize database schema and queries for Simple Bank project

--- README.md
+++ README.md
@@ -11,23 +11,7 @@ Go-Lang, Postgres, Redis, Gin, gRPC, Kubernetes, AWS, CI/CD <br>
 
 ## Database Schema
 This is the database schema for this project: <br>
-![Database Schema](readme/db_scheme.png) <br>
-or you can access is directly [here](https://dbdiagram.io/d/Simple-Bank-678a4ceb6b7fa355c336c916).
-
-### to be continued~# Simple Bank
-
-It will provide APIs for the front-end to do following things:
-1. Create and manage account
-2. Record all balance changes
-3. Money transfer transactions
-
-## What tech this project use?
-Go-Lang, Postgres, Redis, Gin, gRPC, Kubernetes, AWS, CI/CD <br>
-<i>on development...</i>
-
-## Database Schema
-This is the database schema for this project: <br>
-![Database Schema](readme/db_scheme.png) <br>
+![Database Schema](readme/db_schema.png) <br>
 or you can access is directly [here](https://dbdiagram.io/d/Simple-Bank-678a4ceb6b7fa355c336c916).
 
 ### to be continued~
---
+++ util/random.go
@@ -0,0 +1,49 @@
+package util
+
+import (
+	"strings"
+	"time"
+
+	"math/rand"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// RandomInt generates a random integer between min and max
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1) // min->max
+}
+
+// RandomString generates a random string with length n
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for i := 0; i < n; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
+}
+
+// RandomOwner generates a random owner name
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+// RandomMoney generates a random amount of money
+func RandomMoney() int64 {
+	return RandomInt(0, 2000)
+}
+
+// RandomCurrency generates a randon currency
+func RandomCurrency() string {
+	currencies := []string{"EUR", "USD", "SAR"}
+	n := len(currencies)
+	return currencies[rand.Intn(n)]
+}
